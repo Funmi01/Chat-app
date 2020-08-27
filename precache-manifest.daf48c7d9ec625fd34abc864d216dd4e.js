3bfd908f176984883dd6d788bc6082a6efcad3c9
Funmi01 <adeosun.funmi@yahoo.com>
2020-08-28 00:48:02 +0100

    Updates

---
+++ precache-manifest.daf48c7d9ec625fd34abc864d216dd4e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c22ca8faf69f134c73837de1fd307bb",
+    "url": "/Chat-app/index.html/index.html"
+  },
+  {
+    "revision": "4eee9374e23bbcdde75a",
+    "url": "/Chat-app/index.html/static/css/main.50d13cea.chunk.css"
+  },
+  {
+    "revision": "8015b77cb167341d3f34",
+    "url": "/Chat-app/index.html/static/js/2.c7ad47fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Chat-app/index.html/static/js/2.c7ad47fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4eee9374e23bbcdde75a",
+    "url": "/Chat-app/index.html/static/js/main.832e2f64.chunk.js"
+  },
+  {
+    "revision": "b39dc3d5147dfd5d5e39",
+    "url": "/Chat-app/index.html/static/js/runtime-main.8d6c29ba.js"
+  },
+  {
+    "revision": "c75f86dc4978b7aeb705a2ba0bfc23dc",
+    "url": "/Chat-app/index.html/static/media/amie.c75f86dc.jpg"
+  },
+  {
+    "revision": "016112397b8dfd64a932dc57486f02d1",
+    "url": "/Chat-app/index.html/static/media/jake.01611239.jpg"
+  },
+  {
+    "revision": "b90642f25f1db1bbd204b92bbd2b7867",
+    "url": "/Chat-app/index.html/static/media/kelly.b90642f2.jpg"
+  },
+  {
+    "revision": "498633d12eb2ddce3f59fd4e6ecf6dc8",
+    "url": "/Chat-app/index.html/static/media/kourtney.498633d1.jpg"
+  },
+  {
+    "revision": "822bc17112dcba76bf98b4dac10ce2f0",
+    "url": "/Chat-app/index.html/static/media/search.822bc171.svg"
+  },
+  {
+    "revision": "a549da9dafd29af5532dd977e15d1506",
+    "url": "/Chat-app/index.html/static/media/toyosi.a549da9d.jpg"
+  },
+  {
+    "revision": "386cd4a5b2d5caf2bdaf71c29d283042",
+    "url": "/Chat-app/index.html/static/media/tunde.386cd4a5.jpg"
+  },
+  {
+    "revision": "586c0d6881458f0f541acca225c0a84a",
+    "url": "/Chat-app/index.html/static/media/vanessa.586c0d68.jpg"
+  },
+  {
+    "revision": "d747cda2bac9834f4a20c38256e2d000",
+    "url": "/Chat-app/index.html/static/media/vik.d747cda2.jpg"
+  },
+  {
+    "revision": "bae05726c9854bc7c9bd2c51c4a018f8",
+    "url": "/Chat-app/index.html/static/media/you.bae05726.jpg"
+  }
+]);
